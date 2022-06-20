2e4dd81d2b54f1810676b4e8c1969e24aa8e9e6e
vishnuacappella <acappella.studiov@gmail.com>
2022-06-20 15:40:26 +0530

    constructor in class

--- bin/class_dart_example.dart
+++ bin/class_dart_example.dart
@@ -1,10 +1,14 @@
 class Person {
   String? name;
   int age = 10;
+  Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
 }
 
 void main(List<String> arguments) {
-  final person = Person();
-  person.name = 'John';
+  final person = Person('Vishnu', 25);
+  // person.name = 'John';
   print(person.name);
 }
