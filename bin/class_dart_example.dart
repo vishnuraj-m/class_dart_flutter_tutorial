61f104446630a31f3ce315831560e9a6d2cb2e79
vishnuacappella <acappella.studiov@gmail.com>
2022-06-20 15:03:26 +0530

    dart demo code

---
+++ bin/class_dart_example.dart
@@ -0,0 +1,5 @@
+import 'package:class_dart_example/class_dart_example.dart' as class_dart_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${class_dart_example.calculate()}!');
+}
---
+++ test/class_dart_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:class_dart_example/class_dart_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
