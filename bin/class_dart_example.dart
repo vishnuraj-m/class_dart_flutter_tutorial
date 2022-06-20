2665c3eaf699df34aa8e54fe12b305eda4ce1c97
vishnuacappella <acappella.studiov@gmail.com>
2022-06-20 15:09:04 +0530

    decleared name in object

--- bin/class_dart_example.dart
+++ bin/class_dart_example.dart
@@ -1,9 +1,10 @@
 class Person {
-  String name = 'Name';
+  String? name;
   int age = 10;
 }
 
 void main(List<String> arguments) {
   final person = Person();
+  person.name = 'John';
   print(person.name);
 }
